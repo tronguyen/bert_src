2178618262b52e8c2abb7586ef92486ef25d9a72
Trong Nguyen <ntrong@8gpu-4-f1.ai.deakin.edu.au>
2020-11-24 22:57:10 +1100

    add setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup_info = dict(
-    name='graph_embedding',
+    name='bert_src',
     version='1.0',
     url='https://github.com/tronguyen/bert_src.git',
     author='Trong Nguyen',
