c3fbda458564d98d0e63331e7eeb0cce9da13a86
Trong Nguyen <ntrong@8gpu-4-f1.ai.deakin.edu.au>
2020-11-24 22:56:48 +1100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup_info = dict(
+    name='graph_embedding',
+    version='1.0',
+    url='https://github.com/tronguyen/bert_src.git',
+    author='Trong Nguyen',
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "tqdm >= 4.27",
+        "torch==1.5.1",
+        "torchvision==0.6.0"
+    ],
+    description='Modules for graph embedding model',
+    long_description=open('README.md').read(),
+)
+
+setup(**setup_info)
